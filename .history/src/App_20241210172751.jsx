27c2477d8faf9b80e8a122429a0c3cee96e30b36
Jacob Trevino <Jacobatrevino@gmail.com>
2024-12-10 17:42:53 -0700

    jgf

---
+++ .history/src/App_20241210172751.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import styles from "./style";
+import { Navbar, InputForm, Output, Preloader, Footer, Hero } from "./components";
+
+const App = () => {
+  const [result, setResult] = useState(null);
+
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+      
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <InputForm onResult={setResult} />
+          <Output result={result} />
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;å
